b735764cf10527fc09c7c2c2b7e5a3aa29ab745c
Aman Singh <amanksofficial@gmail.com>
2021-05-29 23:26:53 +0530

    zoom functionality and filter added

--- activities/6_cameraApp/activity/script.js
+++ activities/6_cameraApp/activity/script.js
@@ -9,7 +9,9 @@ const constraints = { video: true };
 
 let mediaRecorder,
     chunks = [],
-    recordFlag = false;
+    recordFlag = false,
+    currZoom = 1,
+    filter = "";
 
 //FUNCTIONS
 const recordFn = function () {
@@ -38,23 +40,99 @@ const animateClickBtn = function () {
 };
 
 const captureImg = function () {
-    animateClickBtn;
+    animateClickBtn();
+    const date = new Date();
     let c = document.createElement("canvas");
     c.width = videoEl.videoWidth;
     c.height = videoEl.videoHeight;
     let tool = c.getContext("2d");
+    //origin shifting
+    tool.translate(c.width / 2, c.height / 2);
+    //scaling
+    tool.scale(currZoom, currZoom);
+    //moving back the origin
+    tool.translate(-c.width / 2, -c.height / 2);
     tool.drawImage(videoEl, 0, 0);
+    if (filter != "") {
+        tool.fillStyle = filter;
+        tool.fillRect(0, 0, c.width, c.height);
+        console.log("filled");
+    }
     let link = document.createElement("a");
-    link.download = "image.png";
+    link.download = `${date.toDateString().split(" ").join("-")}.png`;
     link.href = c.toDataURL();
     link.click();
     link.remove();
     c.remove();
 };
 
+const removeFilter = function () {
+    let el = document.querySelector(".on-screen-filter");
+    if (el) {
+        el.remove();
+        filter = "";
+    }
+};
+
+const addFilter = function (color) {
+    let oldFilter = document.querySelector(".on-screen-filter");
+    // console.log(oldFilter);
+    if (oldFilter != null) {
+        // console.log("inside old filter");
+        if (color == oldFilter.style.backgroundColor) {
+            // console.log("inside same color");
+            removeFilter();
+            return;
+        }
+    }
+
+    removeFilter();
+    let filterDiv = document.createElement("div");
+    filterDiv.classList.add("on-screen-filter");
+    filterDiv.style.height = "100vh";
+    filterDiv.style.width = "100vw";
+    filterDiv.style.backgroundColor = `${color}`;
+    console.log(color);
+    filterDiv.style.position = "fixed";
+    filterDiv.style.top = "0px";
+    document.body.appendChild(filterDiv);
+
+    filter = color;
+};
+
 //EVENT LISTENER
 recordEl.addEventListener("click", recordFn);
 clickEl.addEventListener("click", captureImg);
+
+const allFilters = document.querySelectorAll(".camera__filter");
+for (let filter of allFilters) {
+    filter.addEventListener("click", (e) => {
+        let color = e.target.style.backgroundColor;
+        filter = color;
+        addFilter(color);
+    });
+}
+
+const zoomin = document.querySelector(".zoomin");
+const zoomout = document.querySelector(".zoomout");
+
+zoomin.addEventListener("click", () => {
+    let vidScale = Number(videoEl.style.transform.split("(")[1].split(")")[0]);
+    console.log(vidScale);
+    if (vidScale < 3) {
+        currZoom = vidScale + 0.1;
+        videoEl.style.transform = `scale(${currZoom})`;
+    }
+});
+
+zoomout.addEventListener("click", () => {
+    let vidScale = Number(videoEl.style.transform.split("(")[1].split(")")[0]);
+    console.log("hahah");
+    if (vidScale > 1) {
+        currZoom = vidScale - 0.1;
+        videoEl.style.transform = `scale(${currZoom})`;
+    }
+});
 
 // async IIFE for video capture
 (async () => {
